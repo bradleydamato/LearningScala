3a856b3604b7502dcff7d5b0ba581dc593d1e15b
Bradley <bradleydamato@gmail.com>
2020-01-08 17:15:53 -0500

    Call By Name vs Call By Val, good stuff to know

--- src/lectures/part1basics/CBVvsCBN.scala
+++ src/lectures/part1basics/CBVvsCBN.scala
@@ -1,5 +1,17 @@
 package lectures.part1basics
 
-class CBVvsCBN {
+object CBVvsCBN extends App{
+  def CallByValue(x: Long): Unit = {
+    println("by val " + x)
+    println("by val " + x)
+  }
+  def CallByName(x: => Long): Unit = {
+    println("by name " + x)
+    println("by name " + x)
 
+
+
+  }
+  CallByValue(System.nanoTime())
+  CallByName(System.nanoTime())
 }
